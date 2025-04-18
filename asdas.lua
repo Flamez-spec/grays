37b80812d19339f15c099f58e7de5ca11f6d14db
flamez <zadool3444@gmail.com>
2025-04-19 00:48:59 +0700

    Update asdas.lua

--- asdas.lua
+++ asdas.lua
@@ -1,37 +1,23 @@
-local Players = game:GetService("Players")
-local UserInputService = game:GetService("UserInputService")
-local LocalPlayer = Players.LocalPlayer
+local Whitelist = {
+    "Gyraz", -- Имя игрока 1
+    "TreaxxDS", -- Имя игрока 2
+}
 
-local function setEgoValue()
-    if LocalPlayer and LocalPlayer:FindFirstChild("PlayerGui") then
-        local PlayerGui = LocalPlayer.PlayerGui
-        if PlayerGui:FindFirstChild("GeneralGUI") then
-            local GeneralGUI = PlayerGui.GeneralGUI
-            if GeneralGUI:FindFirstChild("EGO") then
-                local EGO = GeneralGUI.EGO
-                if EGO:FindFirstChild("ego") then
-                    EGO.ego.Value = 100
-                    return true
-                end
-            end
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local function isWhitelisted()
+    for _, name in pairs(Whitelist) do
+        if player.Name == name then
+            return true
         end
     end
-        return false
+    return false
 end
 
-UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if not gameProcessed and input.KeyCode == Enum.KeyCode.J then --krch in Enum.KeyCode."key" to the left of krch and then enter there the key that is convenient for you (empty)
-setEgoValue()
+if isWhitelisted() then
+    print("[WHITELIST] Доступ разрешён!")
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Flamez-spec/grays/refs/heads/main/asdas.lua"))() -- Ваш чит-код
+else
+    print("[WHITELIST] Доступ запрещён!")
 end
-end)
-task.spawn(function()
-local success = false
-local attempts = 0
-while not success and attempts < 5 do
-success = setEgoValue()
-attempts += 1
-if not success then
-task.wait(1) 
-end
-end
-end)
